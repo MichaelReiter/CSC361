cb64d05426e64e9f8e22ab5299ec5d2845ef2bbb
Michael Reiter <michaelreiter@live.ca>
2018-01-14 12:10:42 -0800

    Add cookie parsing

--- Assignment 1/smart_client.py
+++ Assignment 1/smart_client.py
@@ -11,7 +11,7 @@ import ssl
 import sys
 from urllib.parse import urlparse
 
-DEBUG_PRINT_ENABLED = True
+DEBUG_PRINT_ENABLED = False
 
 class Cookie:
     def __init__(self, name, key, domain_name):
@@ -32,46 +32,82 @@ def perform_http(uri):
         sys.exit()
 
     http_version = "undetermined"
-    # Try to use HTTP/1.1
+    https_support = "undetermined"
+    cookies = []
+
+    http_2_request = ("GET / HTTP/1.1\r\n" \
+        + "Host: " + uri + "\r\n" \
+        + "Connection: Upgrade, HTTP2-Settings\r\n" \
+        + "Upgrade: h2c\r\n" \
+        + "HTTP2-Settings:\r\n\r\n"
+    ).encode()
+
+    # Try to use HTTP/2
     status_code, response = send_request(
         ip_address=ip_address,
-        request=b"GET / HTTP/1.1\r\n\r\n",
+        request=http_2_request,
         use_https=True
     )
     if status_code == 200:
         # Success
         if DEBUG_PRINT_ENABLED:
-            print("HTTP/1.1 returned 200 OK")
-        http_version = "HTTP/1.1"
+            print("HTTP/2 returned 200 OK")
+        http_version = "HTTP/2"
+        https_support = "yes"
     elif status_code == 302:
-        # Try again with https
-        if DEBUG_PRINT_ENABLED:
-            print("HTTP/1.1 returned 302 Found")            
-    elif status_code == 400:
-        # Bad Request. Try to use HTTP/1.0
-        if DEBUG_PRINT_ENABLED:
-            print("HTTP/1.1 returned 400 Bad Request")        
+        https_support = "no"
+        # Try to use HTTP/1.1
         status_code, response = send_request(
             ip_address=ip_address,
-            request=b"GET / HTTP/1.0\r\n\r\n",
-            use_https=True
+            request=b"GET / HTTP/1.1\r\n\r\n",
+            use_https=False
         )
         if status_code == 200:
             # Success
             if DEBUG_PRINT_ENABLED:
-                print("HTTP/1.0 returned 200 OK")            
-            http_version = "HTTP/1.0"
-        elif status_code == 400:
+                print("HTTP/1.1 returned 200 OK")
+            http_version = "HTTP/1.1"
+        elif status_code == 302:
+            # Try again with https
             if DEBUG_PRINT_ENABLED:
-                print("HTTP/1.0 returned 400 Bad Request")
+                print("HTTP/1.1 returned 302 Found")
+        elif status_code == 400:
+            # Bad Request. Try to use HTTP/1.0
+            if DEBUG_PRINT_ENABLED:
+                print("HTTP/1.1 returned 400 Bad Request")
+            status_code, response = send_request(
+                ip_address=ip_address,
+                request=b"GET / HTTP/1.0\r\n\r\n",
+                use_https=False
+            )
+            if status_code == 200:
+                # Success
+                if DEBUG_PRINT_ENABLED:
+                    print("HTTP/1.0 returned 200 OK")
+                http_version = "HTTP/1.0"
+            elif status_code == 400:
+                if DEBUG_PRINT_ENABLED:
+                    print("HTTP/1.0 returned 400 Bad Request")
+            else:
+                if DEBUG_PRINT_ENABLED:
+                    print("HTTP/1.0 returned unexpected status code " + str(status_code))
         else:
             if DEBUG_PRINT_ENABLED:
-                print("HTTP/1.0 returned unexpected status code " + str(status_code))
-    else:
-        if DEBUG_PRINT_ENABLED:
-            print("HTTP/1.1 returned unexpected status code " + str(status_code))
-    
-    return "not implemented", http_version, [Cookie("-", "asdf", "test.com")]
+                print("HTTP/1.1 returned unexpected status code " + str(status_code))
+
+    cookies = parse_cookies(response)
+
+    return https_support, http_version, cookies
+
+
+def parse_cookies(response):
+    matches = re.finditer(r"Set-Cookie: (.+)=(.+); ((path)=(.+);)? ((domain)=(.+))?", response)
+    cookies = []
+    for match in matches:
+        key = match.group(1)
+        domain = match.group(8)
+        cookies.append(Cookie("-", key, domain))
+    return cookies
 
 
 def send_request(ip_address, request, use_https):
@@ -88,18 +124,19 @@ def send_request(ip_address, request, use_https):
         if DEBUG_PRINT_ENABLED:
             print("Sent request " + str(request) + " to " + ip_address + ":" + str(port) + " with use HTTPS " + str(use_https))
         s.sendall(request)
-        # s.setblocking(False)
+        status_code = -1 # uninitialized
+        response = b""
         while True:
-            response = s.recv(1024*(2**5))
-            if response:
+            more_data = s.recv(1024*(2**5))
+            response += more_data
+            if more_data:
                 if DEBUG_PRINT_ENABLED:
                     print(response.decode())
-                status_code = int(re.search(r"^(HTTP/1.[0|1])\s(\d+)", response.decode()).group(2))
-                return status_code, response.decode()
+                if status_code == -1:
+                    status_code = int(re.search(r"^(HTTP/1.[0|1])\s(\d+)", response.decode()).group(2))
             else:
-                if DEBUG_PRINT_ENABLED:
-                    print('No response')
-                pass
+                break
+        return status_code, response.decode()
 
     except socket.error:
         print("Error sending request. Exiting.")
