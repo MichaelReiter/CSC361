957b3fc67cd8320c0b844c3b51b71463ac81ba99
Michael Reiter <michaelreiter@live.ca>
2018-01-25 15:59:18 -0800

    Refactor directory

--- Assignment 1/smart_client.py
+++ Assignment 1/smart_client.py
@@ -11,183 +11,167 @@ import ssl
 import sys
 from urllib.parse import urlparse
 
-DEBUG_PRINT_ENABLED = True
+MAX_REDIRECTS = 5
 
 class Cookie:
+    '''
+    Cookie parsed from HTTP response header has name, key and domain.
+    '''
     def __init__(self, name, key, domain_name):
         self.name = name
         self.key = key
         self.domain_name = domain_name
-    
+
     def __str__(self):
         return "name: " + self.name + ", key: " + self.key + ", domain name: " + self.domain_name
 
 
-def perform_http(uri):
-    # Try to resolve host's IP address from URI
-    try:
-        ip_address = socket.gethostbyname(uri)
-    except socket.gaierror:
-        print("Error resolving host. Exiting.")
-        sys.exit()
-
-    http_version = "undetermined"
-    https_support = "undetermined"
-    cookies = []
-
-    http_2_request = ("GET / HTTP/1.1\r\n" \
-        + "Host: " + uri + "\r\n" \
-        + "Connection: Upgrade, HTTP2-Settings\r\n" \
-        + "Upgrade: h2c\r\n" \
-        + "HTTP2-Settings:\r\n\r\n"
-    ).encode()
-
-    # Try to use HTTP/2
-    status_code, response = send_request(
-        ip_address=ip_address,
-        request=http_2_request,
-        use_https=False
-    )
-    if status_code == 200:
-        # Success
-        if DEBUG_PRINT_ENABLED:
-            print("HTTP/2 returned 200 OK")
-        http_version = "HTTP/2"
-        https_support = "yes"
-    elif status_code == 302:
-        https_support = "no"
-        # Try to use HTTP/1.1
-        status_code, response = send_request(
-            ip_address=ip_address,
-            request=b"GET / HTTP/1.1\r\n\r\n",
-            use_https=False
-        )
-        if status_code == 200:
-            # Success
-            if DEBUG_PRINT_ENABLED:
-                print("HTTP/1.1 returned 200 OK")
-            http_version = "HTTP/1.1"
-        elif status_code == 302:
-            # Try again with https
-            if DEBUG_PRINT_ENABLED:
-                print("HTTP/1.1 returned 302 Found")
-        elif status_code == 400:
-            # Bad Request. Try to use HTTP/1.0
-            if DEBUG_PRINT_ENABLED:
-                print("HTTP/1.1 returned 400 Bad Request")
-            status_code, response = send_request(
-                ip_address=ip_address,
-                request=b"GET / HTTP/1.0\r\n\r\n",
-                use_https=False
-            )
-            if status_code == 200:
-                # Success
-                if DEBUG_PRINT_ENABLED:
-                    print("HTTP/1.0 returned 200 OK")
-                http_version = "HTTP/1.0"
-            elif status_code == 400:
-                if DEBUG_PRINT_ENABLED:
-                    print("HTTP/1.0 returned 400 Bad Request")
-            else:
-                if DEBUG_PRINT_ENABLED:
-                    print("HTTP/1.0 returned unexpected status code " + str(status_code))
-        else:
-            if DEBUG_PRINT_ENABLED:
-                print("HTTP/1.1 returned unexpected status code " + str(status_code))
-
-    cookies = parse_cookies(response)
-
-    return https_support, http_version, cookies
-
-
-def parse_cookies(response):
-    matches = re.finditer(r"Set-Cookie: (.+)=(.+); ((path)=(.+);)? ((domain)=(.+))?", response)
-    cookies = []
-    for match in matches:
-        key = match.group(1)
-        domain = match.group(8)
-        cookies.append(Cookie("-", key, domain))
-    return cookies
-
-
-def send_request(ip_address, request, use_https):
-    if use_https:
-        port = 443
-    else:
-        port = 80
-
-    s = create_socket(use_https=use_https)
-    s.connect((ip_address, port))
-
-    # Try to send request
-    try:
-        if DEBUG_PRINT_ENABLED:
-            print("Sent request " + str(request) + " to " + ip_address + ":" + str(port) + " with use HTTPS " + str(use_https))
-        s.sendall(request)
-        status_code = -1 # uninitialized
-        response = b""
-        while True:
-            more_data = s.recv(1024*(2**5))
-            response += more_data
-            if more_data:
-                if status_code == -1:
-                    status_code = int(re.search(r"^(HTTP/1.[0|1])\s(\d+)", response.decode()).group(2))
-            else:
-                break
-        if DEBUG_PRINT_ENABLED:
-            print(response.decode()[:1000])
-        return status_code, response.decode("utf-8", "ignore")
-
-    except socket.error:
-        print("Error sending request. Exiting.")
-        sys.exit()
-
-
-def create_socket(use_https):
+def create_socket_and_connect(url, use_https):
+    '''
+    Creates a new socket and connects to url optionally using HTTPS.
+    Returns created socket.
+    '''
     # Try to create socket
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error:
         print("Error creating socket. Exiting.")
         sys.exit()
 
     if use_https:
-        # Try to connect using HTTPS
-        try:
-            ssl_context = ssl.create_default_context()
-            # Try to set application protocols
-            try:
-                protocols = ["h2", "http/1.1", "http/1.0"]
-                ssl_context.set_alpn_protocols(protocols)
-            except NotImplementedError:
-                print("OpenSSL version error. Verify that Python 3 is being used.")
-            s = ssl.wrap_socket(s)
-        except ssl.SSLError:
-            print("SSL error. Exiting.")
-            sys.exit()
+        port = 443
+    else:
+        port = 80
 
-    return s
+    # Try to connect on secure port
+    try:
+        sock.connect((url, port))
+    except socket.error:
+        raise Exception
+
+    if use_https:
+        # Try to wrap socket
+        try:
+            sock = ssl.wrap_socket(sock)
+        except ssl.SSLError:
+            raise Exception
+
+    return sock
+
+
+def send_request(url, path, version, use_https):
+    '''
+    Sends an HTTP request to provided url.
+    Returns response string.
+    '''
+    sock = create_socket_and_connect(url, use_https)
+    request = ("HEAD " + path + " HTTP/" + version + "\r\nHost: " + url + "\r\n\r\n").encode()
+    sock.sendall(request)
+    response = b""
+    while True:
+        try:
+            data = sock.recv(4096)
+            if not data:
+                break
+            response += data
+        except socket.timeout:
+            break
+    return response.decode("utf-8")
+
+
+def parse_cookies(url, response):
+    '''
+    Parses cookies from a url.
+    Returns a list of Cookies.
+    '''
+    cookies = []
+    for match in re.findall(r"Set-Cookie: (.*?)=(.*?);.* (domain=(.*))?", response):
+        d = re.search(r".*?(\..*)", url).group(1)
+        if match[3] == "":
+            domain = d
+        else:
+            domain = match[3]
+        cookies.append(Cookie("-", match[0], domain))
+    return cookies
+
+
+def check_http2_support(url):
+    '''
+    Checks if a URL supports HTTP/2 protocol.
+    Returns True or False.
+    '''
+    ctx = ssl.create_default_context()
+    ctx.set_alpn_protocols(['h2', 'spdy/3', 'http/1.1'])
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn = ctx.wrap_socket(sock, server_hostname=url)
+    conn.connect((url, 443))
+    return conn.selected_alpn_protocol() == "h2"
+
+
+def get_status_code(response):
+    '''
+    Parses a status code from a HTTP response.
+    Returns status code as an integer.
+    '''
+    return int(re.search(r"^(HTTP/1.[0|1])\s(\d+)", response).group(2))
+
+
+def get_http_version(url, response):
+    '''
+    Parses a HTTP version from a HTTP response.
+    Returns string indicating protocol version.
+    '''
+    version = re.search(r"^(HTTP/1.[0|1])\s(\d+)", response).group(1)
+
+    if check_http2_support(url):
+        version = "HTTP/2"
+
+    return version
+
+
+def check_if_supports_https(url):
+    '''
+    Determines if HTTPS is supported.
+    Returns "yes" or "no"
+    '''
+    location = url
+    path = "/"
+    i = 0
+    while i < MAX_REDIRECTS:
+        response = send_request(location, path, "1.1", use_https=True)
+        status_code = get_status_code(response)
+        if status_code in [200, 404, 503, 505]:
+            return "yes", response
+        elif status_code in [301, 302]:
+            i += 1
+            new_location = urlparse(re.search(r"Location: (.*)", response).group(1))
+            location = new_location.netloc
+            path = new_location.path
+            print("Redirected to " + new_location.scheme + "://" + location)
+            if new_location.scheme == "http":
+                return "no", response
+        else:
+            print("Exiting due to unexpected status code: " + str(status_code) + ". Please try rerunning.")
+            sys.exit()
 
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("uri")
-    # args = parser.parse_args()
-    # uri = args.uri
-
-    # unparsed_uri = "web.uvic.ca"
-    # unparsed_uri = "hannahbishop.com"
-    # unparsed_uri = "www.uvic.ca"
-    # parsed_uri = urlparse(unparsed_uri)
-    # uri = parsed_uri.netloc
-
-    # uri = "web.uvic.ca"
-    # uri = "hannahbishop.com"
-    uri = "www.uvic.ca"
-    print("website: " + uri)
-    supports_https, http_version, cookies = perform_http(uri)
+    '''
+    Determines whether a web server supports HTTPS,
+    the newest supported verion of HTTP and parses
+    cookies.
+    '''
+    socket.setdefaulttimeout(3)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    args = parser.parse_args()
+    url = args.url
+    print("website: " + url)
+    supports_https, response = check_if_supports_https(url)
+    newest_http_version = get_http_version(url, response)
+    cookies = parse_cookies(url, response)
     print("1. Support of HTTPS: " + supports_https)
-    print("2. The newest HTTP versions that the web server supports: " + http_version)
+    print("2. The newest HTTP versions that the web server supports: " + newest_http_version)
     print("3. List of Cookies:")
     for cookie in cookies:
         print(cookie)
