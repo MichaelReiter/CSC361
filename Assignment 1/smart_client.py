2f8cc028571e9757720fd0a43ef8619b344efbe8
Michael Reiter <michaelreiter@live.ca>
2018-02-24 00:17:13 -0800

    Clean up a1

--- Assignment 1/smart_client.py
+++ Assignment 1/smart_client.py
@@ -1,36 +1,39 @@
-# University of Victoria
-# Spring 2018
-# CSC 361 Assignment 1
-# Michael Reiter
-# V00831568
+"""
+University of Victoria
+Spring 2018
+CSC 361 Assignment 1
+Michael Reiter
+V00831568
+"""
 
 import argparse
 import re
 import socket
 import ssl
 import sys
+from typing import List
 from urllib.parse import urlparse
 
 MAX_REDIRECTS = 5
 
 class Cookie:
-    '''
+    """
     Cookie parsed from HTTP response header has name, key and domain.
-    '''
-    def __init__(self, name, key, domain_name):
-        self.name = name
-        self.key = key
-        self.domain_name = domain_name
+    """
+    def __init__(self, name: str, key: str, domain_name: str) -> None:
+        self.name: str = name
+        self.key: str = key
+        self.domain_name: str = domain_name
 
-    def __str__(self):
-        return "name: " + self.name + ", key: " + self.key + ", domain name: " + self.domain_name
+    def __str__(self) -> str:
+        return f"name: {self.name}, key: {self.key}, domain name: {self.domain_name}"
 
 
-def create_socket_and_connect(url, use_https):
-    '''
+def create_socket_and_connect(url: str, use_https: bool) -> socket.socket:
+    """
     Creates a new socket and connects to url optionally using HTTPS.
     Returns created socket.
-    '''
+    """
     # Try to create socket
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -59,13 +62,13 @@ def create_socket_and_connect(url, use_https):
     return sock
 
 
-def send_request(url, path, version, use_https):
-    '''
+def send_request(url: str, path: str, version: str, use_https: bool) -> str:
+    """
     Sends an HTTP request to provided url.
     Returns response string.
-    '''
+    """
     sock = create_socket_and_connect(url, use_https)
-    request = ("HEAD " + path + " HTTP/" + version + "\r\nHost: " + url + "\r\n\r\n").encode()
+    request = (f"HEAD {path} HTTP/{version}\r\nHost: {url}\r\n\r\n").encode()
     sock.sendall(request)
     response = b""
     while True:
@@ -79,11 +82,11 @@ def send_request(url, path, version, use_https):
     return response.decode("utf-8")
 
 
-def parse_cookies(url, response):
-    '''
+def parse_cookies(url: str, response: str) -> List[Cookie]:
+    """
     Parses cookies from a url.
     Returns a list of Cookies.
-    '''
+    """
     cookies = []
     for match in re.findall(r"Set-Cookie: (.*?)=(.*?);.* (domain=(.*))?", response):
         d = re.search(r".*?(\..*)", url).group(1)
@@ -95,11 +98,11 @@ def parse_cookies(url, response):
     return cookies
 
 
-def check_http2_support(url):
-    '''
+def check_http2_support(url: str) -> bool:
+    """
     Checks if a URL supports HTTP/2 protocol.
     Returns True or False.
-    '''
+    """
     context = ssl.create_default_context()
     context.set_alpn_protocols(['h2', 'spdy/3', 'http/1.1'])
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -108,19 +111,19 @@ def check_http2_support(url):
     return secure_sock.selected_alpn_protocol() == "h2"
 
 
-def get_status_code(response):
-    '''
+def get_status_code(response: str) -> int:
+    """
     Parses a status code from a HTTP response.
     Returns status code as an integer.
-    '''
+    """
     return int(re.search(r"^(HTTP/1.[0|1])\s(\d+)", response).group(2))
 
 
-def get_http_version(url, response):
-    '''
+def get_http_version(url: str, response: str) -> str:
+    """
     Parses a HTTP version from a HTTP response.
     Returns string indicating protocol version.
-    '''
+    """
     version = re.search(r"^(HTTP/1.[0|1])\s(\d+)", response).group(1)
 
     if check_http2_support(url):
@@ -129,11 +132,11 @@ def get_http_version(url, response):
     return version
 
 
-def check_if_supports_https(url):
-    '''
+def check_if_supports_https(url: str) -> str:
+    """
     Determines if HTTPS is supported.
     Returns "yes" or "no"
-    '''
+    """
     location = url
     path = "/"
     i = 0
@@ -147,31 +150,31 @@ def check_if_supports_https(url):
             new_location = urlparse(re.search(r"Location: (.*)", response).group(1))
             location = new_location.netloc
             path = new_location.path
-            print("Redirected to " + new_location.scheme + "://" + location)
+            print(f"Redirected to {new_location.scheme}://{location}")
             if new_location.scheme == "http":
                 return "no", response
         else:
-            print("Exiting due to unexpected status code: " + str(status_code) + ". Please try rerunning.")
+            print(f"Exiting due to unexpected status code: {status_code}. Please try rerunning.")
             sys.exit()
 
 
-def main():
-    '''
+def main() -> None:
+    """
     Determines whether a web server supports HTTPS,
     the newest supported verion of HTTP and parses
     cookies.
-    '''
+    """
     socket.setdefaulttimeout(3)
     parser = argparse.ArgumentParser()
     parser.add_argument("url")
     args = parser.parse_args()
     url = args.url
-    print("website: " + url)
+    print(f"website: {url}")
     supports_https, response = check_if_supports_https(url)
     newest_http_version = get_http_version(url, response)
     cookies = parse_cookies(url, response)
-    print("1. Support of HTTPS: " + supports_https)
-    print("2. The newest HTTP versions that the web server supports: " + newest_http_version)
+    print(f"1. Support of HTTPS: {supports_https}")
+    print(f"2. The newest HTTP versions that the web server supports: {newest_http_version}")
     print("3. List of Cookies:")
     for cookie in cookies:
         print(cookie)
