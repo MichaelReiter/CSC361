42bd8c0513bc000b456891b14f881905e1bebb23
Michael Reiter <michaelreiter@live.ca>
2018-03-26 13:04:33 -0700

    Add checkpoint

--- Assignment 3/traceroute/parse.py
+++ Assignment 3/traceroute/parse.py
@@ -14,22 +14,36 @@ def read_trace_file(filename: str) -> (str, str, List[str], Dict[int, str]):
         intermediate_ip_addresses = set()
         protocols = {}
         test = []
+        number_of_fragments = 1
+        last_fragment_offset = 0
+        source_ip_address = ""
+        first_packet_id = 0
         for ts, buf in pcap:
             eth = dpkt.ethernet.Ethernet(buf)
             ip = eth.data
             # Filter out non IP packets
             if type(ip) == dpkt.ip.IP:
-                if ip.ttl == 1:
+                if first_packet_id == 0 and ip.ttl == 1:
                     source_ip_address = socket.inet_ntoa(ip.src)
                     destination_ip_address = socket.inet_ntoa(ip.dst)
+                    first_packet_id = ip.id
                 if ip.p in ip_protocol_map:
                     protocols[ip.p] = ip_protocol_map[ip.p]
                 else:
                     protocols[ip.p] = "Unknown protocol"
-                # TTL exceeded: add intermediate (hop) IP address
                 test.append(ip)
+                # TTL exceeded: add intermediate (hop) IP address
                 if type(ip.data) == dpkt.icmp.ICMP and ip.data.type == 11:
                     intermediate_ip_addresses.add(socket.inet_ntoa(ip.src))
-                    # print(f"{ip.ttl}: {socket.inet_ntoa(ip.src)}")
+                if socket.inet_ntoa(ip.src) == source_ip_address:
+                    if ip.mf == 1:
+                        number_of_fragments += 1
+                    else:
+                        last_fragment_offset = ip.off
 
-    return source_ip_address, destination_ip_address, list(intermediate_ip_addresses), protocols
+    return (source_ip_address,
+            destination_ip_address,
+            list(intermediate_ip_addresses),
+            protocols,
+            number_of_fragments,
+            last_fragment_offset)
--- Assignment 3/traceroute/results_logger.py
+++ Assignment 3/traceroute/results_logger.py
@@ -3,7 +3,9 @@ from typing import Dict, List
 def print_results(source_ip_address: str,
                   destination_ip_address: str,
                   intermediate_ip_addresses: List[str],
-                  protocols: Dict[int, str]) -> None:
+                  protocols: Dict[int, str],
+                  number_of_fragments: int,
+                  last_fragment_offset: int) -> None:
     """
     Prints details of a parsed traceroute file, such as IP addresses and protocols
     """
@@ -15,9 +17,14 @@ def print_results(source_ip_address: str,
             print(f"\trouter {i + 1}:\t{address}.")
         else:
             print(f"\trouter {i + 1}:\t{address},")
-    print("The values in the protocol field of IP headers:")
-    for key in sorted(protocols.keys()):
-        print(f"\t{key}:\t{protocols[key]}")
-    # print(f"The number of fragments created from the original datagram is: {None}")
-    # print(f"The offset of the last fragment is: {None}")
-    # print(f"The avg RRT between {None} and {None} is: {None} ms, the s.d. is: {None} ms")
+    # print("\nThe values in the protocol field of IP headers:")
+    # for key in sorted(protocols.keys()):
+    #     print(f"\t{key}:\t{protocols[key]}")
+    # print(f"\nThe number of fragments created from the original datagram is: "
+    #     f"{number_of_fragments}")
+    # print(f"The offset of the last fragment is: {last_fragment_offset}\n")
+    # for address in intermediate_ip_addresses:
+    #     rtt = 0
+    #     stdev = 0
+    #     print(f"The avg RRT between {source_ip_address} and {address} is: {rtt} ms, "
+    #         f"the s.d. is: {stdev} ms")
