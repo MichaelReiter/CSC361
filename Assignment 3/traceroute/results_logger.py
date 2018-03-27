4ef1774d2ef1a63a14e417fd51a61d14180f1e39
Michael Reiter <michaelreiter@live.ca>
2018-03-27 13:52:37 -0700

    Fix RTT logging

--- Assignment 3/traceroute/results_logger.py
+++ Assignment 3/traceroute/results_logger.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 from statistics import mean, pstdev
 
-DECIMAL_PLACES = 5
+DECIMAL_PLACES = 2
 
 def print_results(source_node_ip_address: str,
                   ultimate_destination_ip_address: str,
@@ -20,15 +20,15 @@ def print_results(source_node_ip_address: str,
             print(f"\trouter {i + 1}:\t{address}.")
         else:
             print(f"\trouter {i + 1}:\t{address},")
-    # print("\nThe values in the protocol field of IP headers:")
-    # for key in sorted(protocols.keys()):
-    #     print(f"\t{key}:\t{protocols[key]}")
-    # for fragment_id, fragment in datagrams.items():
-    #     print(f"\nThe number of fragments created from the original datagram "
-    #         f"{fragment_id} is: {fragment.count}")
-    #     print(f"The offset of the last fragment is: {fragment.offset}")
-    # print("")
-    # for destination_ip_address, rtts in round_trip_times.items():
-    #     print(f"The avg RRT between {source_node_ip_address} and {destination_ip_address} is: "
-    #         f"{round(mean(rtts), DECIMAL_PLACES)} ms, "
-    #         f"the s.d. is: {round(pstdev(rtts), DECIMAL_PLACES)} ms")
+    print("\nThe values in the protocol field of IP headers:")
+    for key in sorted(protocols.keys()):
+        print(f"\t{key}:\t{protocols[key]}")
+    for fragment_id, fragment in datagrams.items():
+        print(f"\nThe number of fragments created from the original datagram "
+            f"{fragment_id} is: {fragment.count}")
+        print(f"The offset of the last fragment is: {fragment.offset}")
+    print("")
+    for destination_ip_address, rtts in round_trip_times.items():
+        print(f"The avg RRT between {source_node_ip_address} and {destination_ip_address} is: "
+            f"{round(mean(rtts) * 1000, DECIMAL_PLACES)} ms, "
+            f"the s.d. is: {round(pstdev(rtts) * 1000, DECIMAL_PLACES)} ms")
